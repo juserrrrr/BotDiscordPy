489cd81d50776b31f252b33fe45257ad307baf61
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2021-12-11 14:17:08 -0300

    fix bug workerr

--- events/joinExitServer.py
+++ events/joinExitServer.py
@@ -9,8 +9,8 @@ class JoinServer(commands.Cog):
     async def on_member_join(self,member):
         channelDoor = self.client.get_channel(919076619451269160)
         embed_message = discord.Embed(
-            title = "🏃‍♂️ │ Bem-vindo!",
-            description = f"{member.mention} acaba entrar para o {member.guild.name}.",
+            title = "🎉 │ Bem-vindo!",
+            description = f"{member.mention} acaba de entrar para o {member.guild.name}.",
             color = 0x00FF23
         )
         embed_message.set_thumbnail(url = member.avatar_url_as(format = 'png'))
@@ -20,7 +20,7 @@ class JoinServer(commands.Cog):
     async def on_member_remove(self,member):
         channelDoor = self.client.get_channel(919076619451269160)
         embed_message = discord.Embed(
-            title = "🏃‍♂️ │ Até a proxima!",
+            title = "🏃 │ Até a proxima!",
             description = f"{member.mention} acaba de sair do {member.guild.name}.",
             color = 0xFF0004
         )
