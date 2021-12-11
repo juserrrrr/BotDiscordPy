d4f09d46fc64ff13d72f2cf99305d2c9eb8aadfd
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2021-12-11 14:05:44 -0300

    fix bug workerr

--- index.py
+++ index.py
@@ -16,5 +16,5 @@ for filename in os.listdir('commands'):
     if filename.endswith('.py'):
         client.load_extension(f'commands.{filename[:-3]}')
 
-TOKEN_BOT = config("TOKEN_BOT")
+#TOKEN_BOT = config("TOKEN_BOT")
 client.run(TOKEN_BOT)
