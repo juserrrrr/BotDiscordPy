0f69a3fd8a7803b685dc70cf6674d3f6d18f777f
juserrrrr <gabriel-pontes09@hotmail.com>
2022-12-22 22:28:44 -0300

    Criação do comando usuariolol e também o acrescimo de uma api do lol ao projeto

---
+++ commands/usuarioLol.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from apiLol.apilol import ApiLol
+class UsuarioLol(commands.Cog):
+    def __init__(self,client):
+      self.client = client
+    
+    @app_commands.command(name = 'usuariolol',description="Mostra as informações de determinado usuario do lol")
+    async def usuarioLol(self,interaction: discord.Interaction,usuario:str):
+
+      embed_message = discord.Embed(
+        title = f"🔷 │ **Usuário não encotrado**",
+        description = f"Por favor, tente novamente",
+        color = 0xFF0004
+      )
+      embed_message.set_thumbnail(url = interaction.guild.icon.url)
+
+      apiLol = ApiLol()
+      dados = apiLol.getSummonerName(usuario)
+      if dados.status_code == 200:
+        dados = dados.json()
+        rankedDados = apiLol.getRankedStats(dados['id']).json()
+        rankedDadosSolo = rankedDados[0]
+        rankedDadosFlex = rankedDados[1]
+        soloDuoStats =f"**{rankedDadosSolo.get('tier')} {rankedDadosSolo.get('rank')}**" 
+        flexStats =f"**{rankedDadosFlex.get('tier')} {rankedDadosFlex.get('rank')}**"
+        nomeUsuario = dados.get('name')
+        idIcon = dados.get('profileIconId')
+        urlImage = apiLol.getUrlProfileIcon(idIcon)
+        
+        #EmbedChanged
+        embed_message.add_field(name="Solo/Duo",value=soloDuoStats)
+        embed_message.add_field(name="Flex",value=flexStats)
+        embed_message.set_thumbnail(url= urlImage)
+        embed_message.title = f"🔷 │ **{nomeUsuario}**"
+        embed_message.description = f"**Infomações sobre o jogador**"
+
+      
+      
+      await interaction.channel.send(embed = embed_message)
+      await interaction.response.send_message(embed=discord.Embed(description="Comando executado com sucesso!",color=interaction.guild.me.color),ephemeral=True,delete_after=4)
+
+
+    @usuarioLol.error
+    async def on_usuarioLol_error(self,interaction: discord.Interaction, error: app_commands.AppCommandError):
+      await interaction.guild.get_member(352240724693090305).send(error)
+      await interaction.response.send_message(embed=discord.Embed(description="Aconteceu um erro interno ao executar o comando, o mesmo já foi passado para um responsavel.",color=interaction.guild.me.color),ephemeral=True,delete_after=4)
+    
+async def setup(client):
+    await client.add_cog(UsuarioLol(client))
