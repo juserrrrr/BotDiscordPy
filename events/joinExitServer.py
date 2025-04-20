77c750c98103fa698561e008dafb725e777e2b5d
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2025-04-20 12:57:21 -0300

    refactor: Atualizar mensagens de boas-vindas e despedidas para incluir o nome do membro e alterar o canal de notificação

--- events/joinExitServer.py
+++ events/joinExitServer.py
@@ -10,44 +10,46 @@ class JoinServer(commands.Cog):
 
   @commands.Cog.listener()
   async def on_member_join(self, member):
-    # response = self.timbasService.getWelcomeByServerId(member.guild.id)
-    # msg = response.json().get('welcomeMessage')
-    # description = msg if msg else f"{member.mention} acaba de entrar no {member.guild.name}."
-    # print(description)
-    channelDoor = self.client.get_channel(919076619451269160)
+    channelDoor = self.client.get_channel(1191521944768618517)
     embed_message = discord.Embed(
         title="🎉 │ Bem-vindo(a)!",
-        description=f"{member.mention} acaba de entrar para o {member.guild.name}.",
+        description=f"**{member.name}** acaba de entrar para o {member.guild.name}.",
         color=0x00FF23
     )
     embed_message.set_thumbnail(
         url=member.display_avatar.replace(format='png').url)
-    await channelDoor.send(embed=embed_message)
+    await channelDoor.send(content=f"{member.mention}", embed=embed_message)
 
   @commands.Cog.listener()
   async def on_member_ban(self, guild: discord.Guild, member: discord.Member):
-    channelDoor = self.client.get_channel(919076619451269160)
+    channelDoor = self.client.get_channel(1191521944768618517)
     embed_message = discord.Embed(
         title="⛔ │ Já vai tarde.",
-        description=f"{member.mention} acaba de ser banido do {guild.name}.",
+        description=f"**{member.name}** acaba de ser banido do {guild.name}.",
         color=0xFF0004
     )
     embed_message.set_thumbnail(
         url=member.display_avatar.replace(format='png').url)
-    await channelDoor.send(embed=embed_message)
+    await channelDoor.send(content=f"{member.mention}", embed=embed_message)
 
   @commands.Cog.listener()
   async def on_member_remove(self, member: discord.Member):
-    channelDoor = self.client.get_channel(919076619451269160)
+    channelDoor = self.client.get_channel(1191521944768618517)
+    try:
+      ban = await member.guild.fetch_ban(member)
+      if ban:
+        return  # Se foi banido, não envia a mensagem
+    except discord.NotFound:
+      pass
 
     embed_message = discord.Embed(
-        title="🏃 │ Até a proxima!",
-        description=f"{member.mention} acaba de sair do {member.guild.name}.",
+        title="🏃 │ Até a próxima!",
+        description=f"**{member.name}** acaba de sair do {member.guild.name}.",
         color=0xFF0004
     )
     embed_message.set_thumbnail(
         url=member.display_avatar.replace(format='png').url)
-    await channelDoor.send(embed=embed_message)
+    await channelDoor.send(content=f"{member.mention}", embed=embed_message)
 
 
 async def setup(client):
